2d72582e2e8853f49f76e902c5a5dfbce5608b08
camila-mp <ca.malvp@gmail.com>
2021-11-29 22:15:41 -0300

    cria router com rota de cadastro de usuarios; adiciona usersRouter e errorHandler no app

--- src/api/app.js
+++ src/api/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const usersRouter = require('./controllers/users/router');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
+
 app.use(express.json());
+
+app.use('/users', usersRouter);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use(errorHandler);
 
 module.exports = app;
