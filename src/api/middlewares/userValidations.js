af081612e97765751ec103390e23870da3b9d152
camila-mp <ca.malvp@gmail.com>
2021-11-29 22:11:20 -0300

    cria validaçao para rota de cadastro de usuarios com Joi

---
+++ src/api/middlewares/userValidations.js
@@ -0,0 +1,16 @@
+const Joi = require('joi');
+
+// fonte do regex do email: http://zparacha.com/validate-email-address-using-javascript-regular-expression
+
+const validateUserData = ({ name, email, password }) => {
+  const validation = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().regex(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/).required(),
+    password: Joi.string().required(),
+  }).validate({ name, email, password });
+
+  if (validation.error) return { err: { message: 'Invalid entries. Try again.' } };
+  return true;
+};
+
+module.exports = { validateUserData };
