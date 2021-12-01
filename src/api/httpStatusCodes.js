7e27ab0667896dfb7d306b56ddc7329bb8094b83
camila-mp <ca.malvp@gmail.com>
2021-12-01 10:52:50 -0300

    adiciona mais status ao httpStatusCodes.js

--- src/api/httpStatusCodes.js
+++ src/api/httpStatusCodes.js
@@ -1,9 +1,10 @@
 module.exports = {
   ok: 200,
   created: 201,
+  noContent: 204,
   notFound: 404,
   conflict: 409,
   badRequest: 400,
-  notImplemented: 501,
+  unauthorized: 401,
   internalServerError: 500,
 };
